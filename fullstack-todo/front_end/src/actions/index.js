706d65bea5bab4c5834e8de7a6d8393bd201375a
emzeperix <kpethodaniel@gmail.com>
2020-04-25 10:25:09 +0200

    restructuring projects

--- fullstack-todo/front_end/src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const MASS_ADD_TODO = 'MASS_ADD_TODO';
-export const DEL_TODO = 'DEL_TODO';
-export const CHANGE_FLAG = 'CHANGE_FLAG';
-export const UPDATE_TODO = 'UPDATE_TODO';
